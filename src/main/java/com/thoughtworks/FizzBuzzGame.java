2120a494092a7e1aa7fa7fb0766f879c8220cc79
HwyUee <13164619630@163.com>
2018-04-16 20:52:11 +0800

    build successful

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,14 +5,78 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
-    private List<String> results = new ArrayList<>();
+    private List<String> results = new ArrayList();
+    private int count;
 
     public void start(int count) {
-
+        this.count = count;
     }
 
     public List<String> getResults() {
+        for (int i = 1; i <= count; i++) {
+            if (isContain3(i)) {
+                results.add("Fizz");
+            } else if (isThreeOfTimes(i)) {
+                if (isFixOfTimes(i)) {
+                    if (isSevenOfTimes(i) ) {
+                        results.add("FizzBuzzWhizz");
+                    }
+                    else {
+                        results.add("FizzBuzz");
+                    }
+                }
+                else if (isSevenOfTimes(i)){
+                    results.add("FizzWhizz");
+                }
+                else {
+                    results.add("Fizz");
+                }
+            }else if (isFixOfTimes(i)){
+                if (isSevenOfTimes(i) ) {
+                    results.add("BuzzWhizz");
+                }
+                else {
+                    results.add("Buzz");
+                }
+            }else if (isSevenOfTimes(i)){
+                results.add("Whizz");
+            }
+            else {
+                results.add(i+"");
+            }
+        }
         return results;
+    }
+
+    public boolean isContain3(int i) {
+        int fir = i % 10;
+        int sec = (i / 10) % 10;
+        int tir = i / 10;
+        if (fir == 3 || sec == 3 || tir == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isThreeOfTimes(int i) {
+        if (i % 3 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFixOfTimes(int i) {
+        if (i % 5 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSevenOfTimes(int i) {
+        if (i % 7 == 0) {
+            return true;
+        }
+        return false;
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,38 @@
 package com.thoughtworks;
 
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
 
+    }
+
+    @Test
+    public void testIsContain3() throws Exception {
+        FizzBuzzGame fizzBuzzGame=new FizzBuzzGame();
+        assertTrue(fizzBuzzGame.isContain3(13));
+        assertFalse(fizzBuzzGame.isContain3(15));
+    }
+
+    @Test
+    public void testIsThreeOfTimes() throws Exception {
+        FizzBuzzGame fizzBuzzGame=new FizzBuzzGame();
+        assertTrue(fizzBuzzGame.isThreeOfTimes(15));
+        assertFalse(fizzBuzzGame.isThreeOfTimes(20));
+    }
+
+    @Test
+    public void testIsFixOfTimes() throws Exception {
+        FizzBuzzGame fizzBuzzGame=new FizzBuzzGame();
+        assertTrue(fizzBuzzGame.isFixOfTimes(15));
+        assertFalse(fizzBuzzGame.isFixOfTimes(21));
+    }
+
+    @Test
+    public void testIsSevenOfTimes() throws Exception {
+        FizzBuzzGame fizzBuzzGame=new FizzBuzzGame();
+        assertTrue(fizzBuzzGame.isSevenOfTimes(49));
+        assertFalse(fizzBuzzGame.isSevenOfTimes(64));
     }
 }
